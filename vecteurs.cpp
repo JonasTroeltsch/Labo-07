90b30762835c1ba768b0dfd3a92bd2b923ffeabd
JonasTroeltsch <Jonas.troeltsch@hotmail.com>
2021-12-07 15:07:06 +0100

    vecteurs cpp et h

--- vecteurs.cpp
+++ vecteurs.cpp
@@ -1,6 +1,6 @@
 /*
 -----------------------------------------------------------------------------------
-Nom du fichier : main.cpp
+Nom du fichier : vecteurs.cpp
 Non du labo    : Labo 7 Matrices
 Auteur(s)      : Jonas Troeltsch et Vincent Bruzzese
 Date creation  : 07.12.2021
@@ -12,18 +12,4 @@ Compilateur    : Mingw-w64 g++ 11.2.0
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
-using namespace std;
-
-int main() {
-
-using Ligne = vector<int>;
-using Matrice = vector<Ligne>;
-
- return EXIT_SUCCESS;
-}
-
-bool estCarre(vector<vector<int>> matrice){
-
-   return matrice.size() == matrice.at(0).size();
-}
+#include "vecteurs.h"
---
+++ vecteurs.h
@@ -0,0 +1,8 @@
+//
+// Created by jonas on 7/12/2021.
+//
+
+#ifndef LABO_07_VECTEURS_H
+#define LABO_07_VECTEURS_H
+
+#endif //LABO_07_VECTEURS_H
